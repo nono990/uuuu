b42bfd82ca22ca0f6e40a939b82dda62bc81c490
nono990 <77127534+nono990@users.noreply.github.com>
2021-01-07 23:04:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+// Set the client user's activity
+client.on('ready', () => {
+    client.user.setActivity('😈Mr Harzox😈 !', { type: 'WATCHING' })  
+});
+
+  client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // if the message content starts with "!ban"
+    if (message.content.startsWith('H!ban')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Ban the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           * Read more about what ban options there are over at
+           * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+           */
+          member
+            .ban({
+              reason: 'Ils étaient mauvais!',
+            })
+            .then(() => {
+              // We let the message author know we were able to ban the person
+              message.reply(`Personne à recu son coup de pied ! ${user.tag}`);
+            })
+            .catch(err => {
+              // An error happened
+              // This is generally due to the bot not being able to ban the member,
+              // either due to missing permissions or role hierarchy
+              message.reply('Je n’ai pas été en mesure de bannir le membre');
+              // Log the error
+              console.error(err);
+            });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply("That user isn't in this guild!");
+        }
+      } else {
+        // Otherwise, if no user was mentioned
+        message.reply("Mention please :) !");
+      }
+    }
+  });
+  
+ 
+
+client.login('Nzk2ODMzNjQ2NTgxMzgzMjY4.X_drKQ.litLCUeLCQ4pvpOolYrZ0iE8J78');
